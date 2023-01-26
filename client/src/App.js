5f91f40e07af78685747ce2a06dcd03880241dc1
sanjaidebuger <nsksanjai@gmail.com>
2023-01-26 12:46:06 +0530

    client side update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
-import './App.css';
-import Hello from "./component/hs";
+// import Chat from "./chat/chat";
+import Process from "./process/process";
+import Home from "./component/home/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import io from "socket.io-client";
 
+const socket = io.connect('/');
 
+function Appmain(props) {
+  return (
+    <React.Fragment>
+      <div className="right">
+        <Chat
+          username={props.match.params.username}
+          roomname={props.match.params.roomname}
+          socket={socket}
+        />
+      </div>
+      <div className="left">
+        <Process />
+      </div>
+    </React.Fragment>
+  );
+}
 
 function App() {
   return (
-    
-     <>
-    <Hello />
-     </>
- 
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home socket={socket} />
+          </Route>
+          <Route path="/chat/:roomname/:username" component={Appmain} />
+        </Switch>
+      </div>
+    </Router>
+   
   );
 }
 
